fc8b88e746912233ef9e05f11826b33daaf77880
Sohibjon <83568223+Sohibjon3412@users.noreply.github.com>
2021-10-06 21:51:36 -0700

    add dark mode

---
+++ js/main.js
@@ -0,0 +1,6 @@
+let darkMod = document.querySelector(".header__btn");
+let body = document.querySelector("body");
+
+darkMod.addEventListener("click",function() {
+  body.classList.toggle("add-color");
+});
